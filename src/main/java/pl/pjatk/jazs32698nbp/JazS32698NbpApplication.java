a96be92a21a8670e19833065732b73261a3a8665
piotrszczodrowski <s32698@pjwstk.edu.pl>
2026-06-20 15:07:08 +0200

    initial commit

---
+++ src/main/java/pl/pjatk/jazs32698nbp/JazS32698NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs32698nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS32698NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS32698NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/jazs32698nbp/JazS32698NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs32698nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS32698NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
